4ab141650f595d5ab3268dc28981b2c019510d0c
Leshik1305 <realleshik1305@gmail.com>
2025-12-16 17:49:18 +0300

    create Place model

--- src/events/models.py
+++ src/events/models.py
@@ -1,3 +1,16 @@
+from uuid import uuid4
+
 from django.db import models
 
-# Create your models here.
+class Place(models.Model):
+    """Модель места проведения событий"""
+    id = models.UUIDField(primary_key=True, default=uuid4(), editable=False, verbose_name="ID площадки")
+    name = models.CharField(max_length=255, verbose_name="Название площадки")
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ['-name']
+        verbose_name = 'Площадка'
+        verbose_name_plural = 'Площадки'
+
+
