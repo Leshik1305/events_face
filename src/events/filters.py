08a8be1ec8373f9218ace6a3f1fbed4d554428f7
Leshik1305 <realleshik1305@gmail.com>
2025-12-16 23:29:44 +0300

    add filter by name

---
+++ src/events/filters.py
@@ -0,0 +1,12 @@
+import django_filters
+from .models import Event
+
+
+class EventFilter(django_filters.FilterSet):
+    """Класс фильтрации по названию"""
+
+    name = django_filters.CharFilter(field_name="name", lookup_expr="icontains")
+
+    class Meta:
+        model = Event
+        fields = ["name"]
