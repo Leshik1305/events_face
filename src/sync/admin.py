b6026ce04c884ab070a5f176c4d9abcd3546af75
Leshik1305 <realleshik1305@gmail.com>
2025-12-18 00:16:59 +0300

    add syncResult model in admin

--- src/sync/admin.py
+++ src/sync/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SyncResult
+
+
+@admin.register(SyncResult)
+class SyncResultAdmin(admin.ModelAdmin):
+    list_display = ["id", "created_at", "new_events_count", "updated_events_count"]
+    list_filter = ["created_at"]
+    readonly_fields = ["created_at"]
+    ordering = ["-created_at"]
