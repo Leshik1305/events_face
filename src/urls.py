7d3e99b32cffbdbb074c72db92e6e6079fee0bcd
Leshik1305 <realleshik1305@gmail.com>
2025-12-17 11:14:46 +0300

    add register url

--- src/urls.py
+++ src/urls.py
@@ -21,4 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("src.events.urls")),
+    path("api/auth/", include("users_auth.urls")),
 ]
