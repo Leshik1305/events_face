7144c003295bdf912fbf8635a941ce5ffc724563
Leshik1305 <realleshik1305@gmail.com>
2025-12-17 11:14:18 +0300

    create RegisterView

--- src/users_auth/views.py
+++ src/users_auth/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.db import IntegrityError
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import RegisterSerializer
+
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        serializer = RegisterSerializer()
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                data = serializer.create(serializer.validated_data)
+                return Response(
+                    {
+                        "message": "User created successfully",
+                        "access_token": data["access"],
+                        "refresh_token": data["refresh"],
+                    },
+                    status=status.HTTP_201_CREATED,
+                )
+            except IntegrityError:
+                return Response(
+                    {"error": "Username already exists"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
